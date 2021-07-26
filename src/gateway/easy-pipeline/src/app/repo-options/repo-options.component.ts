01920f211ef5bb838304a33a514f42e6c09cd941
victornaito <victor.naito@hotmail.com>
2021-07-26 17:06:36 -0400

    create form options pipeline

--- src/gateway/easy-pipeline/src/app/repo-options/repo-options.component.ts
+++ src/gateway/easy-pipeline/src/app/repo-options/repo-options.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition, MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-repo-options',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repo-options.component.sass']
 })
 export class RepoOptionsComponent implements OnInit {
+  form: FormGroup;
+  horizontalPosition: MatSnackBarHorizontalPosition = "center";
+  verticalPosition: MatSnackBarVerticalPosition = "top";
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              private snackBar: MatSnackBar) {
+    this.form = this.fb.group({
+      useDockerBuild: [null],
+      dockerPushToContainerRegistry: [null],
+      tests: [null],
+      publishToServer: [null],
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (!this.form.valid) {
+      this.snackBar.open('Cannonball!!', 'Splash', {
+        horizontalPosition: this.horizontalPosition,
+        verticalPosition: this.verticalPosition,
+      });
+
+      return;
+    }
+    this.router.navigateByUrl("details");
   }
 
 }
--- src/gateway/easy-pipeline/src/app/shared/shared.module.ts
+++ src/gateway/easy-pipeline/src/app/shared/shared.module.ts
@@ -5,28 +5,35 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { MatSnackBarModule } from '@angular/material/snack-bar/snack-bar-module';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    FormsModule,
     MatSliderModule,
     MatSelectModule,
     MatCheckboxModule,
     MatButtonModule,
-    FormsModule,
-    MatExpansionModule
+    ReactiveFormsModule,
+    MatExpansionModule,
+    MatSnackBarModule,
+    // MatSnackBarModule,
   ],
   exports: [
     CommonModule,
+    FormsModule,
     MatSliderModule,
     MatSelectModule,
     MatCheckboxModule,
     MatButtonModule,
-    FormsModule,
-    MatExpansionModule
+    ReactiveFormsModule,
+    MatExpansionModule,
+    // MatSnackBarModule,
+    MatSnackBarModule
   ]
 })
 export class SharedModule { }
